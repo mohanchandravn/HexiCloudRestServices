09acefe1b7269b34cefe6e6f00599a383b46b6fd
mohchand <mohchand@MOHCHAND-IN2.idc.oracle.com>
2017-03-17 17:24:49 +0530

    Adding Role based security to Services rest api

--- HexiCloudPortalDBRest/src/com/hexicloud/portaldb/controller/ServiceBenefitsController.java
+++ HexiCloudPortalDBRest/src/com/hexicloud/portaldb/controller/ServiceBenefitsController.java
@@ -1,17 +1,10 @@
 package com.hexicloud.portaldb.controller;
 
 
-import com.hexicloud.portaldb.bean.ClmData;
 import com.hexicloud.portaldb.bean.Usecase;
-import com.hexicloud.portaldb.service.ClmDataService;
-
 import com.hexicloud.portaldb.service.ServiceBenefitService;
 
-import java.math.BigDecimal;
-
 import java.util.List;
-
-import javax.ws.rs.PathParam;
 
 import org.apache.log4j.Logger;
 
@@ -21,11 +14,10 @@ import org.json.simple.parser.JSONParser;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -37,14 +29,10 @@ public class ServiceBenefitsController {
     private ServiceBenefitService serviceBenefitService;
     
     private static final Logger logger = Logger.getLogger(ServiceBenefitsController.class);
-    @ResponseStatus(value = HttpStatus.NOT_FOUND, reason = "Not found in the system")
-    
-    @ExceptionHandler(Exception.class)
-    public void exceptionHandler(Exception ex) {
-	logger.error("Exception is :", ex);
-    }
+
     
     @RequestMapping(value = "/services/rest/serviceBenefits/{serviceName}/", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER')")
     public JSONObject getServiceBenefits(@PathVariable(value = "serviceName")
 							  String serviceName) throws Exception {
 	logger.info("******* Start of getServiceBenefits() in controller ***********");
@@ -56,6 +44,7 @@ public class ServiceBenefitsController {
     }    
     
     @RequestMapping(value = "/services/rest/usecases/{usecaseCode}/", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER')")
     public JSONObject getUsecaseDetails(@PathVariable(value = "usecaseCode")
                                                           String usecaseCode) throws Exception {
         logger.info("******* Start of getUsecaseDetails() in controller ***********");
@@ -67,6 +56,7 @@ public class ServiceBenefitsController {
     }    
     
     @RequestMapping(value = "/services/rest/usecases/", method = RequestMethod.GET)
+    @PreAuthorize("hasRole('USER')")
     public ResponseEntity<List<Usecase>> getUsecases() throws Exception {
         logger.info("******* Start of getUsecases() in controller ***********");
 
