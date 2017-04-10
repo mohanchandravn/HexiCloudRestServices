11df571033733c9d8e0c934a8cd083cee44e89e1
Mohan Chandra <mohan.chandra@oracle.com>
2017-04-10 14:27:05 +0530

    Changing the Admin portal search to get only Users

--- HexiCloudPortalDBRest/src/com/hexicloud/portaldb/daoImpl/UsersDAOImpl.java
+++ HexiCloudPortalDBRest/src/com/hexicloud/portaldb/daoImpl/UsersDAOImpl.java
@@ -134,24 +134,26 @@ public class UsersDAOImpl implements UsersDAO {
         logger.info("Entering method searchUserDetails");
         String query = SqlQueryConstantsUtil.SQL_USER_QUERY;
         StringBuilder whereClause = new StringBuilder();
+        whereClause.append(" WHERE AUTHORITY = 'ROLE_USER'");
         if (!(StringUtils.isEmpty(userId))) {
-            whereClause.append(" WHERE USER_ID = '" + userId + "'");
+//            whereClause.append(" WHERE USER_ID = '" + userId + "'");
+            whereClause.append(" AND USER_ID = '"+ userId + "'");
         }
         if (!(StringUtils.isEmpty(emailId))) {
-            if (whereClause.length() > 0) {
+//            if (whereClause.length() > 0) {
                 whereClause.append(" AND EMAIL = '"+ emailId + "'");
-
-            } else {
-                whereClause.append(" WHERE EMAIL = '"+ emailId + "'");
-            }
+//
+//            } else {
+//                whereClause.append(" WHERE EMAIL = '"+ emailId + "'");
+//            }
         }
         if (!(StringUtils.isEmpty(customerId))) {
-            if (whereClause.length() > 0) {
+//            if (whereClause.length() > 0) {
                 whereClause.append(" AND REGISTRY_ID = '" +customerId + "'" );
-
-            } else {
-                whereClause.append(" WHERE REGISTRY_ID = '" +customerId + "'" );
-            }
+//
+//            } else {
+//                whereClause.append(" WHERE REGISTRY_ID = '" +customerId + "'" );
+//            }
         }
 
         if (whereClause.length() > 0) {
