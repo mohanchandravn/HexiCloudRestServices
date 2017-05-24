8e4316343222f87e93f5630b2178b042fa4f888b
Mohan Chandra <mohan.chandra@oracle.com>
2017-05-24 11:23:39 +0530

    Committing the missing files

--- HexiCloudPortalDBRest/src/com/hexicloud/portaldb/bean/UseCaseDetail.java
+++ HexiCloudPortalDBRest/src/com/hexicloud/portaldb/bean/UseCaseDetail.java
@@ -5,75 +5,17 @@ import java.io.Serializable;
 import java.util.List;
 
 public class UseCaseDetail implements Serializable {
-    @SuppressWarnings("compatibility:-5292630709372001411")
-    private static final long serialVersionUID = 408258975992037223L;
-
     public UseCaseDetail() {
         super();
     }
+    
+    private List<Benefit> benefits;
 
-    private Integer id;
-    private String title;
-    private String shortDesc;
-    private String longDesc;
-    private String image;
-    private List<Service> services = null;
-    private String benefits;
-
-    public Integer getId() {
-        return id;
-    }
-
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getShortDesc() {
-        return shortDesc;
-    }
-
-    public void setShortDesc(String shortDesc) {
-        this.shortDesc = shortDesc;
-    }
-
-    public String getLongDesc() {
-        return longDesc;
-    }
-
-    public void setLongDesc(String longDesc) {
-        this.longDesc = longDesc;
-    }
-
-    public String getImage() {
-        return image;
-    }
-
-    public void setImage(String image) {
-        this.image = image;
-    }
-
-    public List<Service> getServices() {
-        return services;
-    }
-
-    public void setServices(List<Service> services) {
-        this.services = services;
-    }
-
-    public String getBenefits() {
-        return benefits;
-    }
-
-    public void setBenefits(String benefits) {
+    public void setBenefits(List<Benefit> benefits) {
         this.benefits = benefits;
     }
 
+    public List<Benefit> getBenefits() {
+        return benefits;
+    }
 }
